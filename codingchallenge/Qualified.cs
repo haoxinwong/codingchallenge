b863c6960c0aff141ba8ce6d66fa6caca57a1108
haoxinwong <2506891138@qq.com>
2022-01-25 11:01:02 -0500

    1/25

---
+++ codingchallenge/Qualified.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingchallenge
+{
+    class Qualified
+    {
+
+        public static string isQualified(int[]i)
+        {
+            if (i.Length < 8||i.Sum()>120)
+            {
+                return "Disqualified";
+            }
+            int count = 0;
+            int ini = 5;
+            foreach(int num in i)
+            {
+                if (num > ini)
+                {
+                    return "Disqualified";
+                }
+                count++;
+                if (count == 2)
+                {
+                    ini += 5;
+                    count = 0;
+                }
+            }
+            return "Qualified";
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isQualified(new int []{5,5,10,10,15,15,20,20}));
+            Console.WriteLine(isQualified(new int[] { 2, 3, 8, 6, 5, 12, 10, 18 }));
+            Console.WriteLine(isQualified(new int[] { 5, 5, 10, 10, 25, 15, 20, 20 }));
+            Console.WriteLine(isQualified(new int[] { 5, 5, 10, 10, 15, 15, 20 }));
+            Console.WriteLine(isQualified(new int[] { 5, 5, 10, 10, 15, 15, 20, 20 }));
+        }
+    }
+}
---
+++ codingchallenge/SquarePrint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingchallenge
+{
+    class SquarePrint
+    {
+        public static string squarePrint(int i)
+        {
+            string result = "";
+            for (int j = 0; j < i; j++)
+            {
+                for (int t = 0; t < i; t++)
+                {
+                    result += " /\\ ";
+                }
+                result += "\n";
+                for (int m = 0; m < i; m++)
+                {
+                    result += "<  >";
+                }
+                result += "\n";
+                for (int b = 0; b < i; b++)
+                {
+                    result += " \\/ ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(squarePrint(1));
+            Console.WriteLine(squarePrint(2));
+            Console.WriteLine(squarePrint(3));
+
+        }
+    }
+}
